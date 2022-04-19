a5bb1d1044a7e5da5b6fbea642bef718029afa86
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-18 19:49:41 -0500

    Maintain control flow, cd-ing in other functions

--- conf.d/pipenv.fish
+++ conf.d/pipenv.fish
@@ -15,20 +15,24 @@ function __pipenv_shell_activate --on-variable PWD
     end
 
     if not test -n "$PIPENV_ACTIVE"
-        if pipenv --venv >/dev/null 2>&1
-        set -x __pipenv_fish_initial_pwd "$PWD"
+        if test status current-command = "cd" or test status current-command = "__pipenv_shell_activate"
+            if pipenv --venv >/dev/null 2>&1
+            set -x __pipenv_fish_initial_pwd "$PWD"
 
-        if [ "$pipenv_fish_fancy" = 'yes' ]
-            set -- __pipenv_fish_arguments $__pipenv_fish_arguments --fancy
-        end
+            if [ "$pipenv_fish_fancy" = 'yes' ]
+                set -- __pipenv_fish_arguments $__pipenv_fish_arguments --fancy
+            end
 
-        pipenv shell $__pipenv_fish_arguments
+            pipenv shell $__pipenv_fish_arguments
 
-        set -e __pipenv_fish_initial_pwd
-        if test -n "$__pipenv_fish_final_pwd"
-            cd "$__pipenv_fish_final_pwd"
-            set -e __pipenv_fish_final_pwd
-        end
+            set -e __pipenv_fish_initial_pwd
+            if test -n "$__pipenv_fish_final_pwd"
+                cd "$__pipenv_fish_final_pwd"
+                set -e __pipenv_fish_final_pwd
+            end
+            end
+        else
+            . $(pipenv --venv)/bin/activate.fish
         end
     end
 end
